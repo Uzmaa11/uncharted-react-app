be15ee1e6438d0f02ce79590ee823785f20753b4
Uzma Shaikh <2796323S@student.gla.ac.uk>
2026-04-30 15:55:21 +0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,114 @@
+import tour1 from "../src/images/tour-1.jpeg";
+import tour2 from "../src/images/tour-2.jpeg";
+import tour3 from "../src/images/tour-3.jpeg";
+import tour4 from "../src/images/tour-4.jpeg";
+import tour5 from "../src/images/tour-5.jpeg";
+import tour6 from "../src/images/tour-6.jpeg";
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.instagram.com", icon: "fab fa-instagram" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elitAsperiores, officia",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elitAsperiores, officia",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "effortless comfort",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elitAsperiores, officia",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit qui corporis`,
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "september 26th, 2020",
+    title: "Indonesian Adventure",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit qui corporis`,
+    location: "indonesia",
+    duration: 11,
+    cost: 2000,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "november 29th, 2020",
+    title: "Explore Kenya",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit qui corporis`,
+    location: "kenya",
+    duration: 8,
+    cost: 3100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "december 16th, 2020",
+    title: "Honkong Adventure",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit qui corporis`,
+    location: "honkong",
+    duration: 20,
+    cost: 5100,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "january 16th, 2020",
+    title: "African Wild",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit qui corporis`,
+    location: "Africa",
+    duration: 20,
+    cost: 3100,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "april 1st, 2020",
+    title: "Visit India",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit qui corporis`,
+    location: "India",
+    duration: 18,
+    cost: 8100,
+  },
+];
